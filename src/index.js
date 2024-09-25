fa7b3bd4d7d2cb0f56c29e5c853f16b012e24b5b
om mahato <ommahato121@gmail.com>
2024-09-26 00:16:21 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
-export * from './components/Button'
+// export * from './components/Button'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
